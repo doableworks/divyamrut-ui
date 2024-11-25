79627c75c5dd6dcd0dd472490273f9cf4d3b114b
vijayzucol <vijay.kumar@zucolservices.com>
2024-11-25 17:23:40 +0530

    fix/footer for mobile/

--- src/components/common/FooterCom.js
+++ src/components/common/FooterCom.js
@@ -215,8 +215,8 @@ export default function FooterCom() {
         <div className="container mx-auto w-[85%] pt-20">
           {/* <div className="px-4 grid grid-cols-1 md:grid-cols-4 gap-8"> */}
           {/* Logo and Description */}
-          <div className="flex gap-40 pb-16">
-            <div className=" w-[30%]">
+          <div className="flex flex-col md:flex-row gap-20 md:gap-40 pb-16">
+            <div className="w-full md:w-[30%]">
               <div className="flex items-center space-x-2">
                 <div className="w-8 h-8">
                   <img
@@ -282,8 +282,8 @@ export default function FooterCom() {
             </div>
           </div>
           {/* Bottom Bar */}
-          <div className="flex justify-between items-center border-t border-[#FFFFFF2B] py-2  text-center text-green-100 ">
-            <p className="font-inter text-[13px] font-[400] leading-[1.5em] text-left text-[#FFFFFF]">
+          <div className="flex flex-col md:flex-row justify-between items-center border-t border-[#FFFFFF2B] py-2  text-center text-green-100 ">
+            <p className="font-inter text-[13px] font-[400] leading-[1.5em] text-center text-[#FFFFFF]">
               Copyright © {new Date().getFullYear()} Divyamrut, All rights
               reserved. Powered by MoxCreative.
             </p>
