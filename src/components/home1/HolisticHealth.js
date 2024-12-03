bceb3b3495c7ec469b054e2019d0cc1c49a3a579
vijayzucol <vijay.kumar@zucolservices.com>
2024-12-03 12:49:21 +0530

    holistic health animation/stable

--- src/components/home1/HolisticHealth.js
+++ src/components/home1/HolisticHealth.js
@@ -1,74 +1,88 @@
 "use client"; // Use client-side rendering for GSAP animations
 import { Chat, Diamond, PaymentCard, Help } from "@/icon/icons";
 import PlayIcon from "./PlayIcon";
-
-import { useEffect } from "react";
+import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
 
 const HolisticHealth = () => {
-  useEffect(() => {
+  useGSAP(() => {
     // GSAP Animation
     gsap.fromTo(
-      ".hero-content",
-      { opacity: 0, y: 50 },
-      { opacity: 1, y: 0, duration: 1, ease: "power2.out" }
+      ".hero-content-hh",
+      { y: -50 },
+      {
+        y: 0,
+        duration: 1,
+        delay: 0.5,
+        stagger: 0.2,
+        scrollTrigger: {
+          trigger: "#containerHh",
+          start: "top 80%",
+          end: "bottom 20%",
+          scrub: true,
+        },
+      }
     );
   }, []);
 
   return (
-    <section
-      className="relative bg-cover bg-center overflow-hidden h-[45rem] md:h-screen"
-      style={{
-        backgroundImage: "url('/asset/home/natural-health-support.jpg')",
-      }}
-    >
-      <div className="bg-[rgba(0,0,0,.4)] h-full w-full flex items-center justify-center text-white">
-        <div className="hero-content text-center">
-          <div className="flex justify-center items-center mb-12">
-            <div className="w-16 h-16 bg-white rounded-full flex items-center justify-center shadow-lg">
-              <PlayIcon  />
+    <div>
+      <section
+        id="containerHh"
+        className="relative bg-cover bg-center overflow-hidden h-[45rem] md:h-screen"
+        style={{
+          backgroundImage: "url('/asset/home/natural-health-support.jpg')",
+        }}
+      >
+        <div className="h-full hero-content-hh">
+          <div className="bg-[rgba(0,0,0,.4)] h-full w-full flex items-center justify-center text-white">
+            <div className="text-center">
+              <div className="flex justify-center items-center mb-12">
+                <div className="w-16 h-16 bg-white rounded-full flex items-center justify-center shadow-lg">
+                  <PlayIcon />
+                </div>
+              </div>
+              <h2 className="max-w-xl font-suranna text-[32px] md:text-[89px] font-[400] leading-[1em] text-center text-[#FFFFFF] mb-4">
+                Holistic Health, Ancient Wisdom
+              </h2>
+              <p className="max-w-xl mx-auto font-jost text-[16px] md:text-[18px] font-[400] leading-[1.4em] text-center text-[#FFFFFF]">
+                Ultrices quam mattis posuere porttitor tellus rhoncus tristique.
+                Primis aliquam dignissim interdum vel suscipit sodales.
+              </p>
             </div>
           </div>
-          <h2 className="max-w-xl font-suranna text-[32px] md:text-[89px] font-[400] leading-[1em] text-center text-[#FFFFFF] mb-4">
-            Holistic Health, 
-            Ancient Wisdom
-          </h2>
-          <p className="max-w-xl mx-auto font-jost text-[16px] md:text-[18px] font-[400] leading-[1.4em] text-center text-[#FFFFFF]">
-            Ultrices quam mattis posuere porttitor tellus rhoncus tristique.
-            Primis aliquam dignissim interdum vel suscipit sodales.
-          </p>
-        </div>
-      </div>
 
-      <div className="absolute bottom-0 left-0 right-0 bg-black bg-opacity-50 py-4">
-        <div className="grid grid-cols-2 md:grid-cols-4 text-center text-white">
-          <div className="flex flex-col md:flex-row items-center gap-4">
-            <Diamond fill="#99C24A" h={35} w={35} />
-            <p className="font-suranna text-[25px] font-[400] leading-[1.3em] text-text">
-              Trusted Company
-            </p>
-          </div>
-          <div className="flex flex-col md:flex-row items-center gap-4">
-            <Chat fill="#99C24A" h={35} w={35} />
-            <p className="font-suranna text-[25px] font-[400] leading-[1.3em] text-text">
-              Premium Support
-            </p>
-          </div>
-          <div className="flex flex-col md:flex-row items-center gap-4">
-            <PaymentCard fill="#99C24A" h={35} w={35} />
-            <p className="font-suranna text-[25px] font-[400] leading-[1.3em] text-text">
-              Safety Payment
-            </p>
-          </div>
-          <div className="flex flex-col md:flex-row items-center gap-4">
-            <Help fill="#99C24A" h={35} w={35} />
-            <p className="font-suranna text-[25px] font-[400] leading-[1.3em] text-text">
-              Help Center
-            </p>
+          <div className="absolute bottom-0 left-0 right-0 bg-black bg-opacity-50 py-8">
+            <div className="grid grid-cols-2 md:grid-cols-4 text-center text-white">
+              <div className="flex flex-col md:flex-row items-center gap-4">
+                <Diamond fill="#99C24A" h={35} w={35} />
+                <p className="font-suranna text-[25px] font-[400] leading-[1.3em] text-text">
+                  Trusted Company
+                </p>
+              </div>
+              <div className="flex flex-col md:flex-row items-center gap-4">
+                <Chat fill="#99C24A" h={35} w={35} />
+                <p className="font-suranna text-[25px] font-[400] leading-[1.3em] text-text">
+                  Premium Support
+                </p>
+              </div>
+              <div className="flex flex-col md:flex-row items-center gap-4">
+                <PaymentCard fill="#99C24A" h={35} w={35} />
+                <p className="font-suranna text-[25px] font-[400] leading-[1.3em] text-text">
+                  Safety Payment
+                </p>
+              </div>
+              <div className="flex flex-col md:flex-row items-center gap-4">
+                <Help fill="#99C24A" h={35} w={35} />
+                <p className="font-suranna text-[25px] font-[400] leading-[1.3em] text-text">
+                  Help Center
+                </p>
+              </div>
+            </div>
           </div>
         </div>
-      </div>
-    </section>
+      </section>
+    </div>
   );
 };
 
--- src/components/home1/WhoWeAre.js
+++ src/components/home1/WhoWeAre.js
@@ -1,5 +1,5 @@
 "use client";
-import React, { useRef } from "react";
+import React from "react";
 import gsap from "gsap";
 import { useGSAP } from "@gsap/react";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
