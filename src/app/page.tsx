706479da449da870ae658cb7d3f5963df9b3b304
vijayzucol <vijay.kumar@zucolservices.com>
2025-01-06 15:41:08 +0530

    stable/fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,6 +12,7 @@ export default function Home() {
   return (
     <main
     //  className="relative z-10 mt-[-12rem] bg-cover bg-[url('/asset/home/caucasian-woman-having-ayurveda-shirodhara-treatment-in-india.jpg')] bg-no-repeat"
+    className="bg-FFEEE2"
     >
       <Banner />
       {/* <WhoWeAre /> */}
