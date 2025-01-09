3a4c5649c02fde92de9d4ea20a0076afa81c584e
Shubham Arora <shubham.arora@zucolservices.com>
2025-01-09 13:48:52 +0530

    Modified Book Now

--- src/components/BtnCom/BookNow.js
+++ src/components/BtnCom/BookNow.js
@@ -4,7 +4,7 @@ const BookNowBtn = () => {
   return (
     <button
       type="submit"
-      className="site-button-secondary rounded w-full lg:w-[350px] h-[58px] !m-0"
+      className="site-button-primary rounded w-full lg:w-[350px] h-[58px] !m-0"
     >
       Book Now
     </button>
