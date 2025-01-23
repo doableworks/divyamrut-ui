198c47fdb8403b448394c451af393105e0468d1e
zucol-shubham <shubham.arora@zucolservices.com>
2025-01-23 17:54:30 +0530

    Integrated Therapy Detail Page API and Added CommonNotherapy details, FAQs

--- src/app/therapy/page.js
+++ src/app/therapy/page.js
@@ -2,7 +2,7 @@ import AllTherapyList from "@/components/therapy/AllTherapyList";
 
 export default function TherapyPage() {
   return (
-    <div className="p-5">
+    <div className="pb-[6rem] pt-[2rem] lg:pb-[6rem] lg:pt-16">
       <h1 className="section-title !mb-0">Our Therapies</h1>
       <h2 className="highlight-heading">
         Empowering Wellness, Healing,
