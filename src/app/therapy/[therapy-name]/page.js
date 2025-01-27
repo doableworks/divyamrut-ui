8072b0fd07768910b72170d05a11be34e18e466e
zucol-shubham <shubham.arora@zucolservices.com>
2025-01-27 15:54:45 +0530

    Fixed Therapy and Navbar APIURL

--- src/app/therapy/[therapy-name]/page.js
+++ src/app/therapy/[therapy-name]/page.js
@@ -1,34 +1,42 @@
-import TherapyDetail from "@/components/therapy/TherapyDetail";
+// import TherapyDetail from "@/components/therapy/TherapyDetail";
 
-const apiUrl = process.env.NEXT_PUBLIC_API_URL;
+// const apiUrl = process.env.NEXT_PUBLIC_API_URL;
 
-const fetchTherapyDetails = async (therapyName) => {
-  try {
-    const res = await fetch(`${apiUrl}/therapy/therapy-details/${therapyName}`);
-    if (!res.ok) {
-      throw new Error("Failed to fetch data");
-    }
-    const therapyData = await res.json();
-    return therapyData;
-  } catch (error) {
-    console.error("Error fetching therapy data:", error);
-    return null;
-  }
-};
+// const fetchTherapyDetails = async (therapyName) => {
+//   try {
+//     const res = await fetch(`${apiUrl}/therapy/therapy-details/${therapyName}`);
+//     if (!res.ok) {
+//       throw new Error("Failed to fetch data");
+//     }
+//     const therapyData = await res.json();
+//     return therapyData;
+//   } catch (error) {
+//     console.error("Error fetching therapy data:", error);
+//     return null;
+//   }
+// };
 
-const TherapyName = async ({ params }) => {
-  const therapyName = params["therapy-name"];
-  const pageDetails = await fetchTherapyDetails(therapyName);
+// const TherapyName = async ({ params }) => {
+//   const therapyName = params["therapy-name"];
+//   const pageDetails = await fetchTherapyDetails(therapyName);
 
-  if (!pageDetails) {
-    return <div>Error loading therapy details</div>;
-  }
 
-  return (
-    <div>
-      <TherapyDetail details={pageDetails} />
-    </div>
-  );
-};
+//   return (
+//     <div>
+//       <TherapyDetail details={pageDetails} />
+//     </div>
+//   );
+// };
 
-export default TherapyName;
+// export default TherapyName;
+
+
+const page = ({children}) =>{
+  return(
+    <>
+    {children}
+    </>
+  )
+}
+
+export default page
