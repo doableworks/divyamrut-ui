162aa3099a500ab7c445f7a12ed84af61e3b2a9d
zucol-shubham <shubham.arora@zucolservices.com>
2025-01-27 16:07:23 +0530

    Fixed

--- src/app/therapy/[therapy-name]/page.js
+++ src/app/therapy/[therapy-name]/page.js
@@ -1,42 +1,34 @@
-// import TherapyDetail from "@/components/therapy/TherapyDetail";
+import TherapyDetail from "@/components/therapy/TherapyDetail";
 
-// const apiUrl = process.env.NEXT_PUBLIC_API_URL;
+const apiUrl = process.env.NEXT_PUBLIC_API_URL;
 
-// const fetchTherapyDetails = async (therapyName) => {
-//   try {
-//     const res = await fetch(`${apiUrl}/therapy/therapy-details/${therapyName}`);
-//     if (!res.ok) {
-//       throw new Error("Failed to fetch data");
-//     }
-//     const therapyData = await res.json();
-//     return therapyData;
-//   } catch (error) {
-//     console.error("Error fetching therapy data:", error);
-//     return null;
-//   }
-// };
+const fetchTherapyDetails = async (therapyName) => {
+  try {
+    const res = await fetch(`${apiUrl}/therapy/therapy-details/${therapyName}`);
+    if (!res.ok) {
+      throw new Error("Failed to fetch data");
+    }
+    const therapyData = await res.json();
+    return therapyData;
+  } catch (error) {
+    console.error("Error fetching therapy data:", error);
+    return null;
+  }
+};
 
-// const TherapyName = async ({ params }) => {
-//   const therapyName = params["therapy-name"];
-//   const pageDetails = await fetchTherapyDetails(therapyName);
-
-
-//   return (
-//     <div>
-//       <TherapyDetail details={pageDetails} />
-//     </div>
-//   );
-// };
-
-// export default TherapyName;
+const TherapyName = async ({ params }) => {
+  const therapyName = params["therapy-name"];
+  const pageDetails = await fetchTherapyDetails(therapyName);
 
 
-const page = ({children}) =>{
-  return(
-    <>
-    {children}
-    </>
-  )
-}
+  return (
+    <div>
+      {pageDetails?(
+       <TherapyDetail details={pageDetails} />
+      ):null}
+    </div>
+  );
+};
 
-export default page
+export default TherapyName;
+
