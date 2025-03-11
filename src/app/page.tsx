10e6e1ad77e302f73d69b3ed9da2bdab18030bc5
zucol-shubham <shubham.arora@zucolservices.com>
2025-03-11 18:38:43 +0530

    Meta tags Modified:

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,43 @@ import HeroBanner from "@/components/common/reference/HeroBanner";
 import HomeAboutUs from "@/components/home1/HomeAboutUs";
 import HomeEnquiry from "@/components/home1/HomeEnquiry";
 
+export const metadata = {
+  title: 'Nityanava | Holistic Healing & Wellness Therapies',
+  description: 'Experience holistic healing with Osteopathy, Acupuncture, Sound Therapy, Marma & more at Nityanava. Restore balance & enhance well-being naturally',
+  datePublished: "2025-03-11",
+  dateModified: "2025-03-11",
+  robots: 'index, nofollow',
+  openGraph: {
+    type: "article",
+    title: "Nityanava | Holistic Healing & Wellness Therapies",
+    description: "Experience holistic healing with Osteopathy, Acupuncture, Sound Therapy, Marma & more at Nityanava. Restore balance & enhance well-being naturally",
+    images: [
+      {
+        url: "https://desklib.com/desklib-logo.png",  
+        width: 1200,
+        height: 630,
+        alt: "Desklib Logo",
+      },
+    ],
+    url: "https://desklib.com/about-us/",
+    site_name: "Desklib",
+  },
+  twitter: {
+    card: "summary_large_image",
+    site: "@DesklibAI",
+    title: "Nityanava | Holistic Healing & Wellness Therapies",
+    description: "Experience holistic healing with Osteopathy, Acupuncture, Sound Therapy, Marma & more at Nityanava. Restore balance & enhance well-being naturally",
+    image: {
+      url: "https://desklib.com/desklib-logo.png", 
+      alt: "Desklib Logo",
+    },
+  },
+  alternates: {
+    canonical: 'https://desklib.com/',
+  },
+  
+};
+
 export default function Home() {
   return (
     <main>
--- src/app/therapy/[therapy-name]/page.js
+++ src/app/therapy/[therapy-name]/page.js
@@ -7,22 +7,45 @@ const fetchTherapyDetails = async (therapyName) => {
   try {
     const res = await fetch(
       `${apiUrl}/therapy/therapy-details/${therapyName}`,
-      {
-        next: { revalidate: 60 },
-      }
+      { next: { revalidate: 60 } }
     );
 
     if (!res.ok) {
       throw new Error("Failed to fetch data");
     }
 
-    const therapyData = await res.json();
-    return therapyData;
+    return await res.json();
   } catch (error) {
     console.error("Error fetching therapy data:", error);
     return null;
   }
 };
+
+export async function generateMetadata({ params }) {
+  const therapyName = params["therapy-name"];
+  const pageDetails = await fetchTherapyDetails(therapyName);
+
+  if (!pageDetails) return { title: "Therapy Not Found" };
+
+  return {
+    title: pageDetails?.seo_title || "Therapy Details",
+    description: pageDetails?.seo_description || "Learn more about this therapy.",
+    openGraph: {
+      title: pageDetails?.seo_title || "Therapy Details",
+      description: pageDetails?.seo_description || "Learn more about this therapy.",
+      images: [
+        {
+          url: pageDetails?.banner || "https://www.ozassignments.com/assets/OZLogo.png",
+        },
+      ],
+      url: `https://oz-assignments.com/${pageDetails?.slug || ""}`,
+      type: "website",
+    },
+    alternates: {
+      canonical: `https://oz-assignments.com/${pageDetails?.slug || ""}`,
+    },
+  };
+}
 
 const TherapyName = async ({ params }) => {
   const therapyName = params["therapy-name"];
@@ -33,7 +56,7 @@ const TherapyName = async ({ params }) => {
     return null;
   }
 
-  return <div>{pageDetails ? <TherapyDetail data={pageDetails} /> : null}</div>;
+  return <TherapyDetail data={pageDetails} />;
 };
 
 export default TherapyName;
